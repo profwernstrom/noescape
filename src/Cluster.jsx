592cc878a48dc545575fc15fe2299fc9ebed8485
Ogden Wernström <174273094+profwernstrom@users.noreply.github.com>
2024-08-22 08:42:20 +0200

    Use opacity to indicate marker age

--- src/Cluster.jsx
+++ src/Cluster.jsx
@@ -3,6 +3,14 @@ import React, {useEffect} from 'react';
 import {MarkerClusterer} from '@googlemaps/markerclusterer';
 import {formatDate} from "./util.js";
 
+function getMarkerOpacity(date) {
+    if (!date) return 0.5;
+    const age = (new Date() - new Date(date)) / (1000 * 60 * 60 * 24);
+    if (age > 180) return 0.33;
+    if (age > 90) return 0.66;
+    return 1;
+}
+
 function Cluster({cases, onSelectCase}) {
     const map = useMap();
     const markerLibrary = useMapsLibrary('marker');
@@ -12,9 +20,12 @@ function Cluster({cases, onSelectCase}) {
         const markers = cases
             .filter(courtCase => courtCase.position)
             .map(courtCase => {
+                const content = new markerLibrary.PinElement().element;
+                content.style.opacity = getMarkerOpacity(courtCase.arrestDate)
                 const marker = new markerLibrary.AdvancedMarkerElement({
                     position: courtCase.position,
                     title: formatDate(courtCase.arrestDate) || null,
+                    content: content,
                     gmpClickable: true,
                 });
                 marker.addListener("click", () => onSelectCase(courtCase));
