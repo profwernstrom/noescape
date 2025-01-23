c45631dc54e366e9f55adb0687843ae16a41ba45
Ogden Wernström <174273094+profwernstrom@users.noreply.github.com>
2025-01-23 07:38:52 +0100

    Update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   server: {
     strictPort: true, // API key is restricted to port 5173
