89f311c00817ca17dc31a752da120c5df38644b3
Ogden Wernström <174273094+profwernstrom@users.noreply.github.com>
2025-01-25 13:13:55 +0100

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Build doker image
+docker build . -t localhost:32000/noescape:latest
+
+# Create ssh tunnel to the server
+ssh -f -N -L 32000:localhost:32000 noescape.fyi
+
+# Push image to the server through the tunnel
+docker push localhost:32000/noescape:latest
+
+# Remove the tunnel
+pkill -f "ssh -f -N -L 32000"
+
+# Restart the app
+ssh noescape.fyi ./restart
